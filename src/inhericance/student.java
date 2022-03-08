717b4fe505df789bb98373f52ad49ef3afd59231
khangCongNguyen <anhbinhcachu@gmail.com>
2022-03-08 22:47:47 +0700

    doubt note

--- src/inhericance/student.java
+++ src/inhericance/student.java
@@ -1,12 +1,13 @@
 package inhericance;
   
  class student extends Person {
+     //thê, thuộc tính cho student
      String universityName;
      student(String universityName  ,String name, String age, String height) {
 		super(name, age, height);
         this.universityName=universityName;  
 	}
-    
+    //thêm attributr for function
     public void getInfo(){
         super.getInfo();
         System.out.println("university  Name :"+universityName);
